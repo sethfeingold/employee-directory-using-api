56217150994d7a686961d9c1f2aa1cb9a5fa4aed
SethFeingold <sfeingold1104@gmail.com>
2019-12-06 15:04:12 -0600

    generate employecards function works!
    this function now successfully inserts html for 12 random employees and includes their thumbnail photo, name, email, and city!

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const employeeGrid = document.getElementById('employee-grid');
 // Function that fetches employees, url must point to randomuser API
 
 function fetchEmployees(url) {
-    fetch(url)
+    return fetch(url)
         .then(data => data.json())
-        .then(data => console.log(data))
+        .then(data => generateEmployeeCards(data.results))
         .catch(err => console.error(`We're sorry, there was an error fetching your data: ${err.name}`));
 }
 
@@ -18,5 +18,16 @@ fetchEmployees('https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA');
 // Function that generates employee card
 
 function generateEmployeeCards (data) {
-    
+    console.log(data);
+
+    data.forEach( person => console.log(person.name))
+
+    data.forEach( person => employeeGrid.innerHTML += `
+    <div class="employee-card">
+        <img class="employee-thumbnail" src="${person.picture.thumbnail}">
+        <h3 class="employee-name">${person.name.first} ${person.name.last}</h3>
+        <p class="employee-email">${person.email}</p>
+        <p class="employee-city">${person.location.city}</p>
+    </div>
+    `)
 }
