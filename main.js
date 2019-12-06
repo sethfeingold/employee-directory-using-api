6348a20d3200d0a74aad6b19b9a0ab86367a203c
SethFeingold <sfeingold1104@gmail.com>
2019-12-06 14:28:35 -0600

    employee grid variable added to main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // URL that calls the random user api and fetches 12 random employees from US, UK, Australia, France, and Canada
 const twelveEmployeesURL = 'https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA';
 
+//Employee grid section (for inserting employee data)
+const employeeGrid = document.getElementById('employee-grid');
+
 // Function that fetches employees, url must point to randomuser API
 
 function fetchEmployees(url) {
@@ -10,4 +13,10 @@ function fetchEmployees(url) {
         .catch(err => console.error(`We're sorry, there was an error fetching your data: ${err.name}`));
 }
 
-fetchEmployees('https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA')
+fetchEmployees('https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA');
+
+// Function that generates employee card
+
+function generateEmployeeCards (data) {
+    
+}
