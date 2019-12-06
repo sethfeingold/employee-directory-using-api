a3048607babc217365715180e5dc3f14425cebdd
SethFeingold <sfeingold1104@gmail.com>
2019-12-06 13:33:11 -0600

    index.html created, main.js created, basic function for fetching 12 random employees created

---
+++ main.js
@@ -0,0 +1,8 @@
+function fetch12Employees(url) {
+    fetch(url)
+        .then(data => data.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(`We're sorry, there was an error fetching your data: ${err.name}`));
+}
+
+fetch12Employees('https://randomuser.me/api/?results=12');
