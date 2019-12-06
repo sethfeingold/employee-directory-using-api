4ba6d79aab24ce3dc0dfca963517cfc5920b33f8
SethFeingold <sfeingold1104@gmail.com>
2019-12-06 13:51:03 -0600

    separate variable created for URL, to be passed into fetch12Employees function

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-function fetch12Employees(url) {
+// URL that calls the random user api and fetches 12 random employees from US, UK, Australia, France, and Canada
+const twelveEmployeesURL = 'https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA';
+
+// Function that fetches employees, url must point to randomuser API
+
+function fetchEmployees(url) {
     fetch(url)
         .then(data => data.json())
         .then(data => console.log(data))
         .catch(err => console.error(`We're sorry, there was an error fetching your data: ${err.name}`));
 }
 
-fetch12Employees('https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA')
+fetchEmployees('https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA')
