51c184f0f9d44f053f0610605ef09e5a9cee520e
SethFeingold <sfeingold1104@gmail.com>
2019-12-15 15:13:07 -0600

    modal styled, prev and next buttons added for flipping through directory

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // URL that calls the random user api and fetches 12 random employees from US, UK, Australia, France, and Canada
+
 const twelveEmployeesURL = 'https://randomuser.me/api/?results=12&nat=GB,US,AU,FR,CA';
 
 // Employee grid section (for inserting employee data)
+
 const employeeGrid = document.querySelector('#employee-grid');
 
 // Variable to store info for all 12 employees
@@ -21,7 +23,7 @@ function fetchEmployees(url) {
         .catch(err => console.error(`We're sorry, there was an error fetching your data: ${err.name}`));
 }
 
-fetchEmployees('https://randomuser.me/api/?results=12&nat=US');
+fetchEmployees('https://randomuser.me/api/?results=12&nat=US'); // Call fetchEmployees on page load
 
 // Function that generates employee card
 
@@ -39,31 +41,67 @@ function generateEmployeeCards (data) {
     </div>
     `);
 
-    // Setting up event listener for each employee card
+    // Setting up event listener for each employee card, so that modal opens when clicked
 
     document.querySelectorAll('.employee-card').forEach((card, index) => {
         card.addEventListener('click', () => {
           // code to call modal function will go here
-          generateModal(employees[index]);
+          generateModal(employees, employees[index], index);
         });
     });
 }
 
-const generateModal = employee => {
+// Function to generate modal of clicked employee
+
+const generateModal = (employees, employee, index) => {
     const modalContainer = document.querySelector('.modal-container');
     const dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language); // Formats date depending on users locale.
-    
+
     modalContainer.innerHTML = `
       <div class="modal">
         <div class="modal-info-container">
+          <span class='modal-close-x'>x</span>
           <img class="modal-img" src="${employee.picture.large}" alt="${employee.name.first}'s profile picture">
           <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
           <p class="modal-text">${employee.email}</p>
-          <p class="modal-text cap">${employee.location.city}</p><hr>
+          <p class="modal-text cap"><span id='prev-btn'>Prev</span><span class="city">${employee.location.city}</span><span id='next-btn'>Next</span></p><hr>
           <p class="modal-text">${employee.phone}</p>
           <p class="modal-text cap">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.state} ${employee.location.postcode}</p>
           <p class="modal-text">Birthday: ${dob}</p>
         </div>
       </div>
-    `
+    `;
+
+    modalContainer.style.display = 'block';
+
+    // Closes modal when 'x' is clicked
+
+    const modalX = document.querySelector('.modal-close-x');
+
+    modalX.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    })
+
+    // Changes to previous or next employee when 'prev' or 'next' button is clicked
+
+    const nextBtn = modalContainer.querySelector('#next-btn');
+    const prevBtn = modalContainer.querySelector('#prev-btn');
+
+    nextBtn.addEventListener('click', () => {
+        generateModal(employees, employees[index + 1], index + 1);
+    });
+
+    prevBtn.addEventListener('click', () => {
+        generateModal(employees, employees[index - 1], index - 1);
+    });
+
+    // 'Prev' button disappears when first employee is selected, 'Next' button disappears when last employee is selected.
+
+    if (index == 0) {
+        prevBtn.innerHTML = '';
+        document.querySelector('.city').style.paddingLeft = '43px';
+    } else if (index == employees.length - 1) {
+        nextBtn.innerHTML = '';
+        document.querySelector('.city').style.paddingRight = '43px';
+    }
 };
